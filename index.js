b4cda778527c414a726ca123f3c1a4a866ed82e6
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-04-07 06:14:22 +0600

    api become paid, stopped the project here

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 const form = document.querySelector(".inputForm");
+const cardGrid = document.querySelector(".cardGrid");
+
+
+async function generateImage(prompt, quantity) {
+    try {
+        const response = await fetch("https://api.openai.com/v1/images/generations", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                prompt: prompt,
+                n: parseInt(quantity),
+                size: "512x512",
+                response_format: "b64_json"
+            })
+        });
+
+        const { data } = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 
 function handleFormSubmit(e) {
@@ -6,7 +33,17 @@ function handleFormSubmit(e) {
     const prompt = e.srcElement[0].value;
     const quantity = e.srcElement[1].value;
 
-    console.log(prompt, quantity);
+    const cardMockup = Array.from({ length: quantity }, () => {
+        return ` <div style="display:flex;justify-content:center; align-items:center;background:#fff" class="card">
+           <div>
+            <img  src="./assets/Growing_ring.gif" alt="" />
+           </div>
+        </div>
+        `
+    }).join("");
+
+    cardGrid.innerHTML = cardMockup;
+    generateImage(prompt, quantity);
 }
 
 form.addEventListener("submit", handleFormSubmit);
